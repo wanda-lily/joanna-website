05f62d8ee76a61efa89a9dc36cc458d53f259839
wanda-lily <asandatope@gmail.com>
2026-07-11 23:59:36 +0200

    first commit

---
+++ joanna-website/next.config.mjs
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  /* config options here */
+  reactCompiler: true,
+  allowedDevOrigins: ["172.20.10.3", "localhost:3000"],
+}
+
+export default nextConfig
