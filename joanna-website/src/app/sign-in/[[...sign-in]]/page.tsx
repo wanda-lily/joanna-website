46d7632903625e97c82cbe61c55b78c1c9e2f51d
wanda-lily <asandatope@gmail.com>
2026-07-23 22:42:51 +0200

    centre sign in and sign uo

--- joanna-website/src/app/sign-in/[[...sign-in]]/page.tsx
+++ joanna-website/src/app/sign-in/[[...sign-in]]/page.tsx
@@ -1,5 +1,9 @@
 import { SignIn } from "@clerk/nextjs"
 
 export default function SignInPage() {
-  return <SignIn />
+  return (
+    <div className="mx-auto">
+      <SignIn />
+    </div>
+  )
 }
